1a80c35653f104cb6a4c2d19880289e37aa8ca7a
CardboardPL <mrplwaschilling@gmail.com>
2025-09-28 22:11:54 +0800

    feat(init): add initial files

---
+++ helper/isArray.js
@@ -0,0 +1,12 @@
+function isArray(value, direct = true) {
+    if (typeof direct !== 'boolean') throw new Error('Rule value must be a boolean');
+    
+    const isArray = Array.isArray(value);
+    if (direct) {
+        if (!isArray) return { status: false, message: 'Value must be an array' }
+        return { status: true, message: '' };
+    }
+    
+    if (isArray) return { status: false, message: 'Value must not be an array' }
+    return { status: true, message: '' };
+}
---
+++ validate.js
@@ -0,0 +1,45 @@
+import { validateDataType } from "./helper/dataType.js";
+
+const dict = {
+    dataType: validateDataType,
+};
+
+function validate(paramObjArr, checkAllParams = false) {
+    if (!Array.isArray(paramObjArr)) throw new Error('paramObjArr must be an array');
+    if (typeof checkAllParams !== 'boolean') throw new Error('checkAllParams must be a boolean');
+
+    const results = [];
+    for (let i = 0; i < paramObjArr.length; i++) {
+        const paramObj = paramObjArr[i];
+        const checked = new Map();
+        results[i] = { status: true, errors: {} };
+
+        if (typeof paramObj.params !== 'object' || Array.isArray(paramObj.params)) throw new Error('paramObj.params must be an object');
+        for (const param in paramObj.params) {
+            const handlerFunc = dict[param];
+            if (!handlerFunc) throw new Error(`${param} is an invalid parameter`);
+
+            let isParamValid = true;
+            const result = handlerFunc(paramObj.value, paramObj.params[param], checked);
+            if (!result.status) {
+                results[i].status = false;
+                results[i].errors[param] = { param, message: result.message};
+                isParamValid = false;
+            }
+            checked.set(param, isParamValid);
+
+            if (!checkAllParams) break;
+        }
+    }
+
+    return results;
+}
+
+/* Sample paramObjArr
+[{
+  value,
+  params: {
+    param1: val
+  }
+}]
+*/
