ec7e57c68e55559a5a5e9b40acb699e4557229c3
CardboardPL <mrplwaschilling@gmail.com>
2025-10-15 16:10:53 +0800

    feat: add field level validation
    - Implemented field level validation and a new required() rule.

--- current/SchemaField.js
+++ current/SchemaField.js
@@ -1,49 +1,89 @@
+import { ValidatorFunctions } from './Validator.js';
+
 export class SchemaField {
     #dataTypeMethodsLocked = false;
-    orderArray = [];
+    operationsMap = {};
 
     string() {
         if (this.#dataTypeMethodsLocked) throw new Error('Called a locked method');
-        this.orderArray.push({
-            dataType: 'string'
-        });
+        this.operationsMap['dataType'] = 'string';
         this.#dataTypeMethodsLocked = true;
         return this;
     }
 
     number() {
         if (this.#dataTypeMethodsLocked) throw new Error('Called a locked method');
-        this.orderArray.push({
-            dataType: 'number'
-        });
+        this.operationsMap['dataType'] = 'number';
         this.#dataTypeMethodsLocked = true;
         return this;
     }
 
     boolean() {
         if (this.#dataTypeMethodsLocked) throw new Error('Called a locked method');
-        this.orderArray.push({
-            dataType: 'boolean'
-        });
+        this.operationsMap['dataType'] = 'boolean';
         this.#dataTypeMethodsLocked = true;
         return this;
     }
 
     array() {
         if (this.#dataTypeMethodsLocked) throw new Error('Called a locked method');
-        this.orderArray.push({
-            dataType: 'array'
-        });
+        this.operationsMap['dataType'] = 'array';
         this.#dataTypeMethodsLocked = true;
         return this;
     }
 
     object() {
         if (this.#dataTypeMethodsLocked) throw new Error('Called a locked method');
-        this.orderArray.push({
-            dataType: 'object'
-        });
+        this.operationsMap['dataType'] = 'object';
         this.#dataTypeMethodsLocked = true;
         return this;
+    }
+
+    required() {
+        this.operationsMap['required'] = true;
+        this.required = function() {
+            throw new Error('Called a locked method');
+        };
+        return this;
+    }
+
+    validate(data) {
+        const schemaFieldOpMap = this.operationsMap;
+        let isValid = true;
+        const results = {};
+
+        if (schemaFieldOpMap.required && (
+            data == null || data === ''
+        )) {
+            return { status: false, message: 'Field is required' }
+        }
+
+        const handleDataType = (obj) => {
+            const { status, message } = obj;
+            isValid = isValid === false ? isValid : status;
+            results['dataType'] = message
+        }
+
+        switch (schemaFieldOpMap.dataType) {
+            case 'string':
+                handleDataType(ValidatorFunctions.string(data));
+                break;
+            case 'number':
+                handleDataType(ValidatorFunctions.number(data))
+                break;
+            case 'boolean':
+                handleDataType(ValidatorFunctions.boolean(data))
+                break;
+            case 'array':
+                handleDataType(ValidatorFunctions.array(data))
+                break;
+            case 'object':
+                handleDataType(ValidatorFunctions.object(data))
+                break;
+            default:
+                throw new Error('Invalid Data Type');
+        }
+
+        return { status: isValid, messages: results };
     }
 }
